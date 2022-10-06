965abe7b59fd22979dfb50a477933790c62d49cb
Angel Herrarte <angelherrarte3@gmail.com>
2022-10-05 19:59:36 -0600

    Create initial UI

--- src/main/java/com/javaapa/javaapa/FileUploadController.java
+++ src/main/java/com/javaapa/javaapa/FileUploadController.java
@@ -3,12 +3,12 @@ package com.javaapa.javaapa;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class FileUploadController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Uploading file...");
     }
 }
