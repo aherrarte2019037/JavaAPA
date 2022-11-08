38ff70a7a73a8f07131971a1cf6c812ffe8f8970
DanielRasho <ray22933@uvg.edu.gt>
2022-11-07 18:33:01 -0600

    Feature: Let ColorTheme and FontSize enums store the location to the config file for each characteristic.

--- src/main/java/com/javaapa/view/ColorTheme.java
+++ src/main/java/com/javaapa/view/ColorTheme.java
@@ -4,7 +4,18 @@ package com.javaapa.view;
  * Color themes for UI.
  */
 public enum ColorTheme {
-    LIGHT,
-    DEFAULT,
-    DARK
+    LIGHT("themeLight.css"),
+    DEFAULT("themeDefault.css"),
+    DARK("themeDark.css");
+
+    private final String cssFile;
+    private String cssFolder = "css/";
+
+    private ColorTheme(String cssFile){
+        this.cssFile = cssFile;
+    }
+
+    public String getCssFile() {
+        return this.cssFolder + this.cssFile;
+    }
 }
--- src/main/java/com/javaapa/view/FontSize.java
+++ src/main/java/com/javaapa/view/FontSize.java
@@ -4,7 +4,18 @@ package com.javaapa.view;
  * Fontsize options for UI.
  */
 public enum FontSize {
-    SMALL,
-    MEDIUM,
-    BIG
+    SMALL("fontSmall.css"),
+    MEDIUM("fontMedium.css"),
+    BIG("fontBig.css");
+
+    private final String cssFile;
+    private String cssFolder = "css/";
+
+    private FontSize(String cssFile){
+        this.cssFile = cssFile;
+    }
+
+    public String getCssFile() {
+        return this.cssFolder + this.cssFile;
+    }
 }
